c30cc772a7ef04db283696fd750ab5dbc84ab49f
emreiser <emilie.reiser@gmail.com>
2014-01-30 08:47:24 -0500

    Added edit/update article actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,21 @@ class ArticlesController < ApplicationController
 		redirect_to user_path(@user.id)
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.assign_attributes(article_params)
+		if @article.save
+			redirect_to user_article_path(@user, @article)
+		else
+			render :edit
+		end
+
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
