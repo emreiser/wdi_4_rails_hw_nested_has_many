ed80ebe23c79baaae87e3ec7f8c53928b021bb03
emreiser <emilie.reiser@gmail.com>
2014-01-30 09:44:55 -0500

    Oops - switched asc to desc for order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 	def index
 		if params[:user_id]
 			@user = User.find(params[:user_id])
-			@articles = @user.articles.order(created_at: :asc)
+			@articles = @user.articles.order(created_at: :desc)
 		else
 			@articles = Article.order(created_at: :asc)
 		end
